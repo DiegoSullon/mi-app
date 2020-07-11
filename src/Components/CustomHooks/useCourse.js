a3f061913b3e5f7a7c0b0dd3fd67cd06dd560272
Diego Sullon <jdiego.sullon@gmail.com>
2020-07-11 15:09:57 -0500

    Hooks

---
+++ src/Components/CustomHooks/useCourse.js
@@ -0,0 +1,15 @@
+import {useState, useEffect } from 'react';
+import axios from 'axios';
+const useCourse=(id)=>{
+    const [course, setCourse] = useState({    })
+    useEffect(() => {
+        axios.get(`http://my-json-server.typicode.com/diegosullon/json-db/cursos/${id}`).then(
+            resp => {
+                setCourse(resp.data)
+            }
+        )
+    }, [])
+
+    return course
+}
+export default useCourse
--- src/Components/Pages/Course.jsx
+++ src/Components/Pages/Course.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
-const cursos = [
-    { "id": "1", "image": "https://edteam-media.s3.amazonaws.com/courses/original/4d60ef81-2e58-457f-97c7-ee8847663985.jpg", "nombre": "Diego", "price": "$30", "edad": "19" },
-    { "id": "2", "image": "https://edteam-media.s3.amazonaws.com/courses/original/4d60ef81-2e58-457f-97c7-ee8847663985.jpg", "nombre": "Jorge", "price": "$40", "edad": "18" },
-    { "id": "3", "image": "https://edteam-media.s3.amazonaws.com/courses/original/4d60ef81-2e58-457f-97c7-ee8847663985.jpg", "nombre": "Martin", "price": "$10", "edad": "21" }
-]
+import React, { useState } from 'react';
+import useCourse from '../CustomHooks/useCourse';
 const Course = ({ match }) => {
-    const cursoActual = cursos.filter(c => c.id === match.params.id)[0]
+    
+    const [comment, setComment] = useState("Sin comentarios")
+    const course = useCourse(match.params.id)
+   
+    const setMyComment = e =>{
+        setComment(e.target.value)
+    }
     return (
         <div className="ed-grid m-grid-3">
-            {cursoActual ? (
+            {course ? (
                 <>
-                    <h1 className="m-cols-3"> Curso: {cursoActual.nombre} </h1>
-                    <img className="m-cols-1" src={cursoActual.image} alt="Imagen" />
-                    <p className="m-cols2"> {cursoActual.price} </p>
+                    <div className="l-block">
+                        <h1 className="m-cols-3"> Curso: {course.nombre} </h1>
+                        <img className="m-cols-1" src={course.image} alt="Imagen" />
+                        <p className="m-cols2"> {course.price} </p>
+                    </div>
+                    <div className="">
+                        <h2>Escribe tu comentario</h2>
+                        <input type="text" placeholder="Escribe..." onChange={setMyComment.bind(this)}/>
+                        <p>{comment}</p>
+                    </div>
                 </>
             ) : (
                     <h1>El curso no existe</h1>
